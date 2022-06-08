351dbc8947eb4fae3de2d56c6660ec14e057baf8
HomeRight <alen40817a@gmail.com>
2022-06-08 22:09:08 +0800

    update

--- web/src/MyPageOne/redux/actions.js
+++ web/src/MyPageOne/redux/actions.js
@@ -7,54 +7,76 @@ import axios from "axios";
 
 var myaxios = axios.create({
   baseURL: "http://" + process.env.REACT_APP_API_HOST_IP + ":8080",
-  responseType: "json"
+  responseType: "json",
 });
 
 export function getUserList(dispatch) {
-    console.log("action.js - getUserList() called ");
+  console.log("action.js - getUserList() called ");
 
-    // Make a request for a user with a given ID
-    myaxios.get('/demo/all')
+  // Make a request for a user with a given ID
+  myaxios
+    .get("/demo/all")
     .then(function (response) {
-        // handle success
-        console.log(response);
-        dispatch({ type: "get_user", text: "I want to get sth 2. ", users: response.data});
-        // TODO: how to wait for the result to send to reducers ? 
+      // handle success
+      console.log(response);
+      dispatch({
+        type: "get_user",
+        text: "I want to get sth 2. ",
+        users: response.data,
+      });
+      // TODO: how to wait for the result to send to reducers ?
     })
     .catch(function (error) {
-        // handle error
-        console.log(error);
+      // handle error
+      console.log(error);
     })
     .then(function () {
-        // always executed
-    });    
+      // always executed
+    });
 
-    // return { type: "get", text: "I want to get sth 2. " }
+  // return { type: "get", text: "I want to get sth 2. " }
 }
 
 export function addUser(dispatch, name) {
-    console.log("action.js - addUser() called ");
-    console.log("action.js - addUser() called name: " , name);
+  console.log("action.js - addUser() called ");
+  console.log("action.js - addUser() called name: ", name);
 
-    // Make a request for a user with a given ID
+  // Make a request for a user with a given ID
 
-    myaxios.post('/demo/add?name=' + name + '&email=' + name + '@gmail.com', {})
-      .then(function (response) {
-        console.log(response);
+  myaxios
+    .post("/demo/add?name=" + name + "&email=" + name + "@gmail.com", {})
+    .then(function (response) {
+      console.log(response);
 
-        getUserList(dispatch);
+      getUserList(dispatch);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
-    // return { type: "get", text: "I want to get sth 2. " }
+  // return { type: "get", text: "I want to get sth 2. " }
 }
 
+export function removeUser(dispatch, id) {
+  console.log("action.js - removeUser() called ");
+  console.log("action.js - removeUser() called id: ", id);
 
-  
+  // Make a request for a user with a given ID
+
+  myaxios
+    .get("/demo/remove/" + id)
+    .then(function (response) {
+      console.log(response);
+
+      getUserList(dispatch);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  // return { type: "get", text: "I want to get sth 2. " }
+}
+
 // export function increment(index) {
 //     return { type: "increment", text: "I want to increment sth." }
 // }
-  
--- web/src/MyPageOne/redux/container.js
+++ web/src/MyPageOne/redux/container.js
@@ -1,31 +1,33 @@
-import { connect } from 'react-redux'
-import { getUserList } from './actions'
-import { addUser } from './actions'
+import { connect } from "react-redux";
+import { getUserList } from "./actions";
+import { addUser } from "./actions";
+import { removeUser } from "./actions";
 // import TodoList from '../components/TodoList'
-import UIComponent from '../component';
+import UIComponent from "../component";
 
-const mapStateToProps = state => {
-  console.log("container.js - mapStateToProps() called - state: " , state);    
+const mapStateToProps = (state) => {
+  console.log("container.js - mapStateToProps() called - state: ", state);
   return {
-    reduxState: state, // this will expose too many information to the client 
-    myCount: state.MyPageOneState.count
-  }
-}
+    reduxState: state, // this will expose too many information to the client
+    myCount: state.MyPageOneState.count,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   console.log("container.js - mapDispatchToProps() called");
   return {
-      // when you call dispatch( 'your action result data' ), Redux will send the action result to the list of reducers you added at the very beginning. 
-      getUserList: () => getUserList(dispatch)
-      ,addUser: (name) => addUser(dispatch, name)
-      // getSth: () => dispatch(getSth())
-      // ,get123: () => dispatch({ type: 'INCREMENT' })
-  }
-}
+    // when you call dispatch( 'your action result data' ), Redux will send the action result to the list of reducers you added at the very beginning.
+    getUserList: () => getUserList(dispatch),
+    addUser: (name) => addUser(dispatch, name),
+    removeUser: (id) => removeUser(dispatch, id),
+    // getSth: () => dispatch(getSth())
+    // ,get123: () => dispatch({ type: 'INCREMENT' })
+  };
+};
 
 const MyPageOneContainer = connect(
   mapStateToProps,
   mapDispatchToProps
-)(UIComponent)
+)(UIComponent);
 
-export default MyPageOneContainer
+export default MyPageOneContainer;
